dbca81d12aae79f95492bef96494de2a730bd3bb
YeonSeong-Lee <dltjddus3@gmail.com>
2022-02-05 22:46:19 +0900

    chagne seperatfrom '_' to ' '

--- RandomNameGenerator.js
+++ RandomNameGenerator.js
@@ -5,7 +5,7 @@ const randomName = (NickNameData) => {
     NickNameData.determiners[
       Math.floor(Math.random() * NickNameData.determiners.length)
     ] +
-    "_" +
+    " " +
     NickNameData.animals[
       Math.floor(Math.random() * NickNameData.animals.length)
     ]
